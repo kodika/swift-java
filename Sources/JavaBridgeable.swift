9097fedf0b5dd7cf9ab63ec4977cccebbd1a7d1c
Kostas Antonopoulos <kostas@devpro.gr>
2021-06-03 16:28:07 +0300

    [ADD] Support all Arrays with Codable elements

--- Sources/JavaBridgeable.swift
+++ Sources/JavaBridgeable.swift
@@ -43,7 +43,8 @@ extension Set: JavaBridgeable where Element: Codable {}
 extension Date: JavaBridgeable {}
 extension Data: JavaBridgeable {}
 extension URL: JavaBridgeable {}
-extension Array: JavaBridgeable where Element == UInt8 {} //for ByteArray support
+extension Array: JavaBridgeable where Element: Codable {}
+//extension Array: JavaBridgeable where Element == UInt8 {} //for ByteArray support
 
 extension String: JavaBridgeable {
 
